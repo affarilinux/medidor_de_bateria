982cefe1e38ad54fa8aff5a03f7b24307862985a
antonio waghtom <waghtompereira@gmail.com>
2021-07-06 16:20:57 -0300

    retirada variavel fixa label main.py

--- BATERIA/main.py
+++ BATERIA/main.py
@@ -26,7 +26,7 @@ from informacao_bateria  import informacao_carregamento
 
 #from memoria_ram         import cal
 
-from main_variavel_fixa                   import Label_fixo
+from main_variavel_fixa  import Label_fixo # janela principal variaveis fixas label
 
 ###############################################
 # inicio janela
@@ -81,7 +81,7 @@ class Window  (Label_fixo, QMainWindow):
           self.Label_Funcao_Janela      () 
           self.Botao_janela             () 
 
-          #self.Variaveis_fixa_Sistema   ()     # varaveis fixa: tempo bateria, estado bateria, memoria ram
+          
           
           self.Uso_Sistema_bateria      ()     # estado da bateria e uso da bateria
           #self.Sistema_Ram_Por_centagem ()     # quantidade de uso do sistema ram
@@ -240,28 +240,7 @@ class Window  (Label_fixo, QMainWindow):
 
      def Label_Funcao_Janela             (self):
 
-          '''#label fixa
-          self.label_porcentagem  = QLabel     (self)
-
-          self.label_porcentagem.setText       (" BATERIA:" )
-
-          self.label_porcentagem.setStyleSheet ('QLabel { font: bold; font-size:15px; background-color: #00FF00}') #cor Line
-
-          self.label_porcentagem.move          (260,5)          #x,y externo
-          self.label_porcentagem.resize        (105,20)         #x,y interno
-
-          #label fixa
-          self.label_porcentagem  = QLabel     (self)
-
-          self.label_porcentagem.setText       ("%" )
-
-          self.label_porcentagem.setStyleSheet ('QLabel { font: bold; font-size:35px; background-color: #00FF00}') #cor Line
-
-          self.label_porcentagem.move          (330,30)          #x,y externo
-          self.label_porcentagem.resize        (35,35)           #x,y interno'''
-
-          #fat = janela()
-
+          
           at = med
           self.medida = str(at)
 
@@ -298,61 +277,7 @@ class Window  (Label_fixo, QMainWindow):
      def Entrar_Janela_Sistema          (self):
           print("ola")
 
-     ##########################################################################################
-     ##########################################################################################
-     ##### variaveis fixa do sistema
-
-     '''def Variaveis_fixa_Sistema         (self):
-
-          ###################################################################
-          ##### tempo bateria
-
-          #label fixa
-          self.label_inf_bateria  = QLabel     (self)
-
-          self.label_inf_bateria.setText       (" TEMPO:" )
-
-          self.label_inf_bateria.setStyleSheet ('QLabel { font: bold; font-size:15px; background-color: #F5DEB3}') # cor: Wheat
-
-          self.label_inf_bateria.move          (10,5)                    #x,y externo
-          self.label_inf_bateria.resize        (80,30)                   #x,y interno
-
-          ###################################################################
-          ##### estado bateria
-
-          #label fixa
-          self.label_tempo_carga  = QLabel     (self)
-
-          self.label_tempo_carga.setText       (" ESTADO:" )
-
-          self.label_tempo_carga.setStyleSheet ('QLabel { font: bold; font-size:15px; background-color: #F5DEB3 }') # cor: Wheat
-
-          self.label_tempo_carga.move          (10,40)                   #x,y externo
-          self.label_tempo_carga.resize        (80,30)                   #x,y interno
-
-          ###################################################################
-          ##### memoria ram
-
-          #label fixa
-          self.label_nome_ram  = QLabel        (self)
-
-          self.label_nome_ram.setText          (" RAM:" )
-
-          self.label_nome_ram.setStyleSheet    ('QLabel { font: bold; font-size:15px; background-color: #00BFFF}') # cor: Wheat
-
-          self.label_nome_ram.move             (260,70)                    #x,y externo
-          self.label_nome_ram.resize           (105,20)                     #x,y interno
-
-          #label fixa
-          self.label_nome_ram  = QLabel        (self)
-
-          self.label_nome_ram.setText          ("%" )
-
-          self.label_nome_ram.setStyleSheet    ('QLabel { font: bold; font-size:25px; background-color: #00BFFF}') # cor: Wheat
-
-          self.label_nome_ram.move             (335,95)                    #x,y externo
-          self.label_nome_ram.resize           (30,25)                     #x,y interno'''
-
+     
     
 
 
