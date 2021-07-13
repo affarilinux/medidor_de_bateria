923b4199b1cd3b7a2781c28463509accce494113
antonio waghtom <waghtompereira@gmail.com>
2021-07-13 20:24:03 -0300

    config_main
    atualização do codico

--- BATERIA/config_main.py
+++ BATERIA/config_main.py
@@ -4,40 +4,12 @@
 
 ''' tamanho da janela principal'''
 
+Titulo = " MEDIDOR DE BATERIA "  
+
 largura = 370
 altura  = 125
 
 #widgets - informações - setext
 
 por_centagem_label = '%'
-
-
-
-
-
-
-
-
-
-
-	
-	
-
-		
-
-		
-
-	
-
-		
-		
-
-	
-
-
-	
-		
-		
-
-
 
