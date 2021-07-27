403bcdc308f690e6ba1fdd9aaf1decbdd8da5d3b
antonio waghtom <waghtompereira@gmail.com>
2021-07-27 14:29:44 -0300

    adição da janela auxiliar no main.py

--- BATERIA/main.py
+++ BATERIA/main.py
@@ -11,7 +11,7 @@ import  sys
 
 from PyQt5.QtWidgets  import ( QApplication, QMainWindow, QPushButton )
 from PyQt5.QtGui      import QIcon
-from PyQt5.QtCore     import pyqtSlot, QObject, pyqtSignal
+from PyQt5.QtCore     import pyqtSlot, QObject, pyqtSignal, QTimer
 
 
 ##################################################
@@ -33,8 +33,15 @@ from CONFIGURACAO.config_sec       import LARGURA_2, ALTURA_2
 
 #arquivo.py- Bateria
 from config_multi_janelas           import ( QICONE_BARRA_DE_TAREFA, TITULO, 
-                                             COR_DARKGRAY )
+                                             COR_DARKGRAY 
+                                            )
 
+
+##################################################
+### configurações da janela auxiliar
+
+from CONFIGURACAO.config_janela_auxiliar import LARGURA_3, ALTURA_3
+from TEMPORIZADOR.timer                  import CHAMADA_5000
 
 ##################################################
 ### arquivos.py da janela principal
@@ -43,10 +50,12 @@ from config_multi_janelas           import ( QICONE_BARRA_DE_TAREFA, TITULO,
 from JANELA.JANELA_PRINCIPAL.main_variavel_fixa     import Janela1Fixa   
 ## relogio de tempo de carga
 from JANELA.JANELA_PRINCIPAL.main_tempo             import ( 
-     jANELA1TemporizadorCarga )
+     jANELA1TemporizadorCarga 
+)
 ## carregando ou descarregando True/False
 from JANELA.JANELA_PRINCIPAL.main_estado_bateria    import (
-    Janela1EstadoCargaBateria )
+    Janela1EstadoCargaBateria 
+)
 ## calculo de porcentagem ram
 from JANELA.JANELA_PRINCIPAL.main_calculo_ram       import Janela1CalculoRam           
 ## nivel de carga da bateria
@@ -72,6 +81,8 @@ from JANELA.JANELA_PRINCIPAL.CONFIGURACOES_PRIMARIA.dimensionamento import (
 #variaveis fixas
 from JANELA.JANELA_SECUNDARIA.sec_variavel_fixa    import Janela2Fixa
 
+##################################################
+### arquivos.py da janela auxiliar
 
 #-------------------------------------------------
 ##################################################
@@ -87,9 +98,34 @@ class Sinais ( QObject ):
     sinal1 = pyqtSignal( name = "Janelaprincipal"  ) 
 
 
+class JanelaAuxialiar ( QMainWindow ):
+    def __init__( self ):
+
+        super ().__init__()  # metodo construtor
+
+        self.TOP_3  = 700
+        self.LEFT_3 = 350
+
+        self.chamar()
+
+    def chamar(self):
+
+        # titulo
+        self.setWindowTitle  ( TITULO )
+
+        # informações da janela
+        self.setGeometry     ( self.TOP_3, self.LEFT_3, LARGURA_3, ALTURA_3 )
+
+        self.setFixedSize    ( LARGURA_3, ALTURA_3 ) 
+
+        # cor de fundo 
+        self.setStyleSheet   ( COR_DARKGRAY )                         
+
+
 #-------------------------------------------------
 ##################################################
 ### janela secundaria
+
 class JanelaSegunda ( Janela2Fixa, QMainWindow ):
 
     def __init__( self ):
@@ -103,7 +139,6 @@ class JanelaSegunda ( Janela2Fixa, QMainWindow ):
         ###chamar função
 
         self.Init_janela()
-
 
      #############################################
      ######## execução de janela
@@ -212,6 +247,7 @@ class Window ( Janela1Fixa, jANELA1TemporizadorCarga,
 
         # chamadado botao da janela
         self.Janela_a_Botao()
+        self.Loop_Parte1()
          
 
      #############################################
@@ -248,6 +284,39 @@ class Window ( Janela1Fixa, jANELA1TemporizadorCarga,
         # disconectar o sinal de abrir janela
         self.sinal_jan_1.sinal1.disconnect () 
 
+    ##########################################
+    ## janela auxiliar
+
+    def Loop_Parte1(self):
+
+        self.TIMER_auxiliar = QTimer        ( self )
+
+        self.TIMER_auxiliar.setInterval     ( CHAMADA_5000 )
+        self.TIMER_auxiliar.start           ()
+
+        #chamada de funçãO
+        self.TIMER_auxiliar.timeout.connect ( self.Janela_Auxiliar_Chamada )
+
+    def Janela_Auxiliar_Chamada (self):
+
+        # informação bateria
+
+        self.MINIMO_BATERIA = 20
+        self.MAXIMO_BATERIA = 90
+        
+        # informação processador
+
+        self.NIVEL_PROCESSADOR = 90
+
+        # informação ram
+
+        self.NIVEL_RAM   = 90
+
+        ## chama a janela auxiliar
+        if self.MINIMO_BATERIA == 20:
+            self.tela_3 = JanelaAuxialiar()
+            self.tela_3.show()
+
 
 #-------------------------------------------------
 ##################################################
@@ -258,7 +327,6 @@ class Controlador:
     def __init__( self ):
 
         self.Show_Login()
-
 
      #janela principal
     @pyqtSlot()                 # decoreto
@@ -273,7 +341,6 @@ class Controlador:
 
         #self.tela_2.close()
 
-
      #janela secundaria
      #@pyqtSlot ()
     def Show_2( self ):
@@ -287,7 +354,7 @@ class Controlador:
         # abrir janela principal
         self.tela_2.show()                     
 
-          
+    
 #-------------------------------------------------
 ##################################################
 ###execução main da janela
@@ -304,4 +371,3 @@ def main():
 if __name__ == "__main__":
 
     main ()
-
